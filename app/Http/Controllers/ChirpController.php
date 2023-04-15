be474bccaff492e30df9a6268afbb8dd423e362f
israelcena <israelsjm@gmail.com>
2023-04-15 08:55:05 -0300

    edit Chirp With Policy and Validation

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,75 +2,79 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreChipRequest;
 use App\Models\Chirp;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\View\View;
 
 class ChirpController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(): View
-    {
-        return view('chirps.index', [
-            'chirps' => Chirp::with('User')->latest()->get(),
-        ]);
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index(): View
+  {
+    return view('chirps.index', [
+      'chirps' => Chirp::with('User')->latest()->get(),
+    ]);
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request): RedirectResponse
-    {
-        $validated = $request->validate([
-            'message' => 'required|string|max:255',
-        ]);
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(StoreChipRequest $request): RedirectResponse
+  {
+    $requestChirp = $request->all();
+    $request->user()->chirps()->create($requestChirp);
 
-        $request->user()->chirps()->create($validated);
+    return redirect(route('chirps.index'))->with('status-message', 'The Chirp has created');
+  }
 
-        return redirect(route('chirps.index'));
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(Chirp $chirp)
+  {
+    //
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Chirp $chirp)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(Chirp $chirp): View
+  {
+    $this->authorize('update', $chirp);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Chirp $chirp)
-    {
-        //
-    }
+    return view('chirps.edit', [
+      'chirp' => $chirp,
+    ]);
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Chirp $chirp)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(StoreChipRequest $storeChipRequest, Chirp $chirp): RedirectResponse
+  {
+    $this->authorize('update', $chirp);
+    $chirp->update($storeChipRequest->all());
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Chirp $chirp)
-    {
-        //
-    }
+    return redirect(route('chirps.index'))->with('status-message', 'The Chirp was updated');
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Chirp $chirp)
+  {
+    //
+  }
 }
---
+++ resources/views/components/alert-top.blade.php
@@ -0,0 +1,18 @@
+@if (session()->has('status-message'))
+    <div class="mb-3 w-full items-center rounded-lg bg-warning-100 px-6 py-5 text-base text-warning-800 data-[te-alert-show]:inline-flex"
+        role="alert" data-te-alert-init data-te-alert-show>
+        <strong class="mr-1">{{ session()->get('status-message') }}! </strong> Congrats.
+        <button type="button"
+            class="ml-auto box-content rounded-none border-none p-1 text-warning-900 opacity-50 hover:text-warning-900 hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+            data-te-alert-dismiss aria-label="Close">
+            <span
+                class="w-[1em] focus:opacity-100 disabled:pointer-events-none disabled:select-none disabled:opacity-25 [&.disabled]:pointer-events-none [&.disabled]:select-none [&.disabled]:opacity-25">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6">
+                    <path fill-rule="evenodd"
+                        d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
+                        clip-rule="evenodd" />
+                </svg>
+            </span>
+        </button>
+    </div>
+@endif
